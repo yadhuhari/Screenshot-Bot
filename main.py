2574291f78ec49fd557012e5019f42c7f975b738
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-04-26 17:23:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 import random
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import ForceReply
+from pyrogram.types import CallbackQuery
 
 API_ID = "23050566"
 API_HASH = "25e954ccd4afb778eea69bd6754275ff"
@@ -39,7 +41,23 @@ Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ: [×͜× 𝙼𝚛. 𝙷𝙺𝚉 𝚃𝙶 🇮🇳�
             )
     )
      
-    
+
+@Bot.on_callback_query(
+    filters.create(lambda _, __, query: query.data.startswith("mscht"))
+)
+async def _(c, m):
+    await m.answer()
+    dur = m.message.text.markdown.split("\n")[-1]
+    await m.message.delete(True)
+    await c.send_message(
+        m.from_user.id,
+        f"#manual_screenshot\n\n{dur}\n\nNow send your list of seconds separated by `,`(comma).\nEg: `0,10,40,60,120`."
+        "\nThis will generate screenshots at 0, 10, 40, 60, and 120 seconds. \n\n"
+        "1. The list can have a maximum of 10 valid positions.\n"
+        "2. The position has to be greater than or equal to 0, or less than the video length in order to be valid.",
+        reply_to_message_id=m.message.reply_to_message.message_id,
+        reply_markup=ForceReply(),
+    )    
 
 
 print("Bot Started..!")
